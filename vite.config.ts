4b00ba2fe224a21a3f1ea5b6fb001bf697455073
jharo0714 <jharo0714@gmail.com>
2026-05-01 08:47:39 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -17,6 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
+  base: '/familyfirstresidentialmanagement/', // fix for blank page
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
